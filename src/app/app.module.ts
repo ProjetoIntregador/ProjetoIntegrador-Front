5f5f52df666d6f5450433f6a65824a8dfbc9723a
kaique-mota <kaiquecarvalho42724@gmail.com>
2021-07-28 13:33:33 -0300

    adicionando componente rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
