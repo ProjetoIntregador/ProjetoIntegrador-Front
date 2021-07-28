d5e2c71230959408d67d9c0e3f34f63b3212b993
blendagaspar <blendagaspar@hotmail.com>
2021-07-28 14:03:41 -0300

    Adicionando o componente menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
