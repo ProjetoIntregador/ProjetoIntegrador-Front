8283f7980435a79b3aeae787e37c05c2d75e4dec
delis-edwirges <delis.edwirges@gmail.com>
2021-07-28 14:41:21 -0300

    Criando componente postagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { PostagensComponent } from './postagens/postagens.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntrarComponent
+    EntrarComponent,
+    PostagensComponent
   ],
   imports: [
     BrowserModule,
